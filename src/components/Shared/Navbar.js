e28d0997889b6e8bcc4dda781eadca0d0d5e4d5c
adityawintech <aditya#19AI>
2025-08-12 23:48:41 +0530

    Aditya

--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -36,7 +36,7 @@ export default function Navbar() {
           </Link>
 
           {/* Desktop Navigation */}
-          <div className="hidden md:flex items-center space-x-8">
+          <div className="hidden md:flex items-center space-x-5">
             {navLinks.map((link) => (
               <Link
                 key={link.href}
